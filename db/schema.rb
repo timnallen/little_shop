2fe93b830c25a7978ce0449f0fd98a5ee721d27a
plapicola <p.lapicola@gmail.com>
2019-02-09 11:46:50 -0700

    Adds default value to Orders#status
    Co-authored-by: Tim Allen <42568362+allen4397@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209184245) do
+ActiveRecord::Schema.define(version: 20190209184525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
