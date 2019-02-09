f2e79edb92d36604468adc418dc8f461d79c47c6
plapicola <p.lapicola@gmail.com>
2019-02-09 11:08:40 -0700

    Added users table and migration. Adds spec files for users and tests for basic validations.
    Co-authored-by: Tim Allen <42568362+allen4397@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe 'validations' do
+  end
+  describe 'relationships' do
+  end
+  describe 'instance methods' do
+  end
+  describe 'class methods' do
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :password}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zipcode}
+
+    it {should validate_uniqueness_of(:email)}
+
+    it {should validate_numericality_of(:zipcode)
+        .is_at_least(0)
+        .is_less_than(100000)
+        .only_integer
+        }
+
+    it {should validate_length_of(:state)
+        .is_equal_to(2)
+    }
+
+    it {should validate_length_of(:name)
+        .is_at_least(1)
+    }
+
+    it {should validate_length_of(:email)
+        .is_at_least(1)
+    }
+
+    it {should validate_length_of(:password)
+        .is_at_least(1)
+    }
+
+    it {should validate_length_of(:address)
+        .is_at_least(1)
+    }
+
+    it {should validate_length_of(:city)
+        .is_at_least(1)
+    }
+
+  end
+  describe 'relationships' do
+  end
+  describe 'instance methods' do
+  end
+  describe 'class methods' do
+  end
+end
