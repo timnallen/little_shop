605922bb474cb17c0ec314b27ae0d1212fcdef9d
plapicola <p.lapicola@gmail.com>
2019-02-09 11:43:23 -0700

    Adds tests for Order model and creates migration
    Co-authored-by: Tim Allen <42568362+allen4397@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209180457) do
+ActiveRecord::Schema.define(version: 20190209184245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 20190209180457) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "orders", "users"
 end
