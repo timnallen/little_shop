919858744b8153a835d4f67103a826c3e7b52f54
plapicola <p.lapicola@gmail.com>
2019-02-09 12:46:57 -0700

    Adds disabled column to users table
    Co-authored-by: Tim Allen <42568362+allen4397@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209191622) do
+ActiveRecord::Schema.define(version: 20190209194437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20190209191622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "registered"
+    t.boolean "disabled", default: false
   end
 
   add_foreign_key "items", "users"
