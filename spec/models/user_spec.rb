a14e7b42a158066df57e66add4f1c097f913c737
plapicola <p.lapicola@gmail.com>
2019-02-09 12:46:29 -0700

    Adds tests for presence of disabled flag
    Co-authored-by: Tim Allen <42568362+allen4397@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :state}
     it {should validate_presence_of :zipcode}
     it {should validate_presence_of :role}
+    it {should validate_presence_of :disabled}
 
     it {should validate_uniqueness_of(:email)}
 
