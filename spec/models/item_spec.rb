9fd53aafede02a39d403ebed4d61e98240074a05
plapicola <p.lapicola@gmail.com>
2019-02-09 12:10:40 -0700

    Adds tests for item model
    Co-authored-by: Tim Allen <42568362+allen4397@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,11 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe 'validations' do
+    it {should validate_presence_of :quantity}
+    it {should validate_presence_of :price}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :image}
+    it {should validate_presence_of :disabled}
+    it {should validate_presence_of :description}
+
+    it {should validate_numericality_of(:quantity)
+      .only_integer
+      .greater_than_or_equal_to(0)
+    }
+
+    it {should validate_numericality_of(:price)
+      .greater_than_or_equal_to(0)
+    }
+
+    it {should validate_length_of(:name)
+        .is_at_least(1)
+    }
+
+    it {should validate_length_of(:description)
+        .is_at_least(1)
+    }
   end
+
   describe 'relationships' do
+    it {should have_many :order_items}
+    it {should belong_to :user}
   end
+
   describe 'instance methods' do
   end
+
   describe 'class methods' do
   end
 end
