c81a06596f94592166db9871f51937d884efb294
plapicola <p.lapicola@gmail.com>
2019-02-10 14:13:15 -0700

    Adds tests for email error flash if user enters an email that is already registered in the database
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -25,8 +25,7 @@ RSpec.describe 'When I click on register in the nav bar' do
       end
 
       it 'which will send an error message if I enter invalid information' do
-        visit welcome_index_path
-        click_link 'Register'
+        visit register_path
 
         fill_in 'user[zipcode]', with: 'abc'
         click_button 'Register'
@@ -34,6 +33,26 @@ RSpec.describe 'When I click on register in the nav bar' do
         expect(current_path).to eq(users_path)
 
         expect(page).to have_content('The information you entered was invalid.')
+      end
+
+      it 'which will display a message if I enter a existing email' do
+        tim = build(:user)
+        tim.save
+
+        visit register_path
+
+        fill_in 'user[name]', with: 'John Smith'
+        fill_in 'user[address]', with: '123 Main Street'
+        fill_in 'user[city]', with: 'Denver'
+        fill_in 'user[state]', with: 'CO'
+        fill_in 'user[zipcode]', with: '12345'
+        fill_in 'user[email]', with: tim.email
+        fill_in 'user[password]', with: 'abc123'
+        fill_in 'user[password_confirmation]', with: 'abc123'
+        click_button 'Register'
+
+        expect(current_path).to eq(users_path)
+        expect(page).to have_content('That email is already registered.')
       end
     end
   end
