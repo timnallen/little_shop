66923c29d2e67f8c9b213c67f67f2e39aa2a0a82
plapicola <p.lapicola@gmail.com>
2019-02-10 11:57:14 -0700

    Adds remaining views for login and registration. Upgrades navbar to bootstrap.
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :render_nav
-
-  private
-
-  def render_nav
-    render partial: "nav/visitor"
-  end
 end
