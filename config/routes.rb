f8923919f08428f91dea09385567d310b22f6575
Tim Allen <tnelsallen3@gmail.com>
2019-02-12 11:39:09 -0700

    Built user login functionality and incomplete nav bar partials
    Co-authored by: Peter Lapicola <p.lapicola@gmail.com>
    Co-authored by: Teresa Knowles <13354855+teresa-m-knowles@users.noreply.github.com>
    Co-authored by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   resources :welcome, only: :index
   resources :items, only: :index
   resources :users, only: [:index, :create]
-  resources :sessions, only: :create
 
   get '/cart', to: 'cart#show'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/dashboard', to: 'merchant/users#show'
   get '/register', to: 'users#new'
   get '/profile', to: 'registered/users#show'
 end
