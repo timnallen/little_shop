bff0a41263b7cf0b4679c856ca5efd1c4c003cdf
Tim Allen <tnelsallen3@gmail.com>
2019-02-12 17:32:33 -0700

    Added specs for item index, all items, info and link functionality
    Co-authored by: Peter Lapicola <p.lapicola@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :welcome, only: :index
-  resources :items, only: :index
+  resources :items, only: [:index, :show]
   resources :users, only: [:index, :create]
 
   get '/cart', to: 'cart#show'
