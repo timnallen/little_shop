1bf6a4a1e5215efe48af8890802e1f74604cfee4
plapicola <p.lapicola@gmail.com>
2019-02-13 21:34:50 -0700

    Adds test for worst_items method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -71,5 +71,33 @@ RSpec.describe Item, type: :model do
       expect([top_items[3].name, top_items[3].quantity]).to eq([item_2.name, order_items[1].quantity])
       expect([top_items[4].name, top_items[4].quantity]).to eq([item_1.name, order_items[0].quantity])
     end
+
+    it 'worst_items' do
+      merchant = build(:merchant)
+      merchant.save
+      user = build(:user)
+      user.save
+      order = user.orders.create
+      item_1 = merchant.items.create!(name: "Item 1", description: "Description 1", price: 1.11, quantity: 10, image: "test.png")
+      item_2 = merchant.items.create(name: "Item 2", description: "Description 2", price: 2.22, quantity: 10, image: "test.png")
+      item_3 = merchant.items.create(name: "Item 3", description: "Description 3", price: 3.33, quantity: 10, image: "test.png")
+      item_4 = merchant.items.create(name: "Item 4", description: "Description 4", price: 4.44, quantity: 10, image: "test.png")
+      item_5 = merchant.items.create(name: "Item 5", description: "Description 5", price: 5.55, quantity: 10, image: "test.png")
+      order_items = []
+      order_items << OrderItem.create(order: order, item: item_1, unit_price: item_1.price, quantity: 1)
+      order_items << OrderItem.create(order: order, item: item_2, unit_price: item_2.price, quantity: 2)
+      order_items << OrderItem.create(order: order, item: item_3, unit_price: item_3.price, quantity: 3)
+      order_items << OrderItem.create(order: order, item: item_4, unit_price: item_4.price, quantity: 4)
+      order_items << OrderItem.create(order: order, item: item_5, unit_price: item_5.price, quantity: 5)
+      order_items.each {|order_item| order_item.update(fulfilled: true)}
+
+      top_items = Item.worst_items(5)
+
+      expect([top_items[0].name, top_items[0].quantity]).to eq([item_1.name, order_items[0].quantity])
+      expect([top_items[1].name, top_items[1].quantity]).to eq([item_2.name, order_items[1].quantity])
+      expect([top_items[2].name, top_items[2].quantity]).to eq([item_3.name, order_items[2].quantity])
+      expect([top_items[3].name, top_items[3].quantity]).to eq([item_4.name, order_items[3].quantity])
+      expect([top_items[4].name, top_items[4].quantity]).to eq([item_5.name, order_items[4].quantity])
+    end
   end
 end
