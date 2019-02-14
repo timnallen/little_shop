55a0abcc30a3ac80d28b7d304cca588d9188392c
plapicola <p.lapicola@gmail.com>
2019-02-13 21:33:41 -0700

    Adds remaining logic to view and controller for displaying worst items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.enabled_items
     @top_items = Item.top_items(5)
+    @worst_items = Item.worst_items(5)
   end
 
   def show
