122d91ad89176dd44e2f2d4f49c20fda4be5927a
plapicola <p.lapicola@gmail.com>
2019-02-14 22:50:41 -0700

    Corrects test ID issue and adds new item link to page

--- spec/features/merchants/items/merchant_item_index_spec.rb
+++ spec/features/merchants/items/merchant_item_index_spec.rb
@@ -56,24 +56,24 @@ RSpec.describe 'when I visit /merchants/items' do
 
     it 'I see a button to disable an enabled and ordered item' do
       enabled_ordered_item = create(:order_item)
-      enabled_ordered_item.item = @enabled_items.first
+      enabled_ordered_item.update(item: @enabled_items.first)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
       visit merchant_items_path
 
-      within "#item-#{enabled_ordered_item.id}" do
+      within "#item-#{@enabled_items.first.id}" do
         expect(page).to have_button("Disable Item")
       end
     end
 
     it 'I see a button to enable a disabled and ordered item' do
       disabled_ordered_item = create(:order_item)
-      disabled_ordered_item.item = @disabled_item
+      disabled_ordered_item.update(item: @disabled_item)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
       visit merchant_items_path
 
-      within "#item-#{disabled_ordered_item.id}" do
+      within "#item-#{@disabled_item.id}" do
         expect(page).to have_button("Enable Item")
       end
     end
