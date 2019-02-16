89de3fcf0dfc9e968d8e8775a1f95ce598ff79f9
Tim Allen <tnelsallen3@gmail.com>
2019-02-16 08:30:36 -0700

    Adds order show spec that it shows all info about an order

--- spec/features/orders/order_show_spec.rb
+++ spec/features/orders/order_show_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe 'order show page', type: :feature do
       @item_1 = create(:item)
       @item_2 = create(:item)
       @order = create(:order, user: @user)
-      create(:order_item, order: @order, item: @item_1)
-      create(:order_item, order: @order, item: @item_2)
+      @order_item_1 = create(:order_item, order: @order, item: @item_1)
+      @order_item_2 = create(:order_item, order: @order, item: @item_2)
 
       login_as(@user)
     end
@@ -20,19 +20,38 @@ RSpec.describe 'order show page', type: :feature do
 
       expect(current_path).to eq(profile_order_path(@order))
     end
+
+    it 'shows me all information about the order' do
+      visit profile_order_path(@order)
+
+      expect(page).to have_content(@order.id)
+      expect(page).to have_content(@order.created_at)
+      expect(page).to have_content(@order.updated_at)
+      expect(page).to have_content(@order.status)
+
+      within "#item-#{@item_1.id}" do
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content(@item_1.description)
+        expect(page).to have_content(@item_1.image)
+        expect(page).to have_content(@order_item_1.quantity)
+        expect(page).to have_content(@order_item_1.unit_price)
+        expect(page).to have_content(@order_item_1.subtotal)
+      end
+
+      within "#item-#{@item_2.id}" do
+        expect(page).to have_content(@item_2.name)
+        expect(page).to have_content(@item_2.description)
+        expect(page).to have_content(@item_2.image)
+        expect(page).to have_content(@order_item_2.quantity)
+        expect(page).to have_content(@order_item_2.unit_price)
+        expect(page).to have_content(@order_item_2.subtotal)
+      end
+
+      expect(page).to have_content(@order.quantity_of_items)
+      expect(page).to have_content(@order.grand_total)
+    end
   end
 
   describe 'as a visitor' do
   end
 end
-
-# And I click on a link for order's show page
-# My URL route is now something like "/profile/orders/15"
-# I see all information about the order, including the following information:
-# - the ID of the order
-# - the date the order was made
-# - the date the order was last updated
-# - the current status of the order
-# - each item I ordered, including name, description, thumbnail, quantity, price and subtotal
-# - the total quantity of items in the whole order
-# - the grand total of all items for that order
