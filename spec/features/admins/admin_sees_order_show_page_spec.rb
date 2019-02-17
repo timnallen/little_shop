a650cf5e5e52fc4204eb85c79035eaaceb5ae633
Tim Allen <tnelsallen3@gmail.com>
2019-02-16 18:41:45 -0700

    Adds spec for all contents on admin order show page

--- spec/features/admins/admin_sees_order_show_page_spec.rb
+++ spec/features/admins/admin_sees_order_show_page_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe 'admin views order show' do
     before :each do
       @user = create(:user)
       @order = create(:order, user: @user)
-      merchant = create(:merchant)
-      item_1 = create(:item, user: merchant)
-      item_2 = create(:item, user: merchant)
-      order_item_1 = create(:order_item, item: item_1, order: @order)
-      order_item_2 = create(:order_item, item: item_2, order: @order)
+      @merchant = create(:merchant)
+      @item_1 = create(:item, user: @merchant)
+      @item_2 = create(:item, user: @merchant)
+      @order_item_1 = create(:order_item, item: @item_1, order: @order)
+      @order_item_2 = create(:order_item, item: @item_2, order: @order)
+
       admin = create(:admin)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -25,18 +26,34 @@ RSpec.describe 'admin views order show' do
       expect(current_path).to eq(admin_order_path(@order))
     end
     #
-    # it 'shows me all the information about the order' do
-    #   visit admin_order_path(@order)
-    #
-    #
-    # end
+    it 'shows me all the information about the order' do
+      visit admin_order_path(@order)
+
+      expect(page).to have_content(@order.id)
+      expect(page).to have_content(@order.created_at)
+      expect(page).to have_content(@order.updated_at)
+      expect(page).to have_content(@order.status)
+
+      within "#item-#{@item_1.id}" do
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content(@item_1.description)
+        expect(page).to have_content(@item_1.image)
+        expect(page).to have_content(@order_item_1.quantity)
+        expect(page).to have_content(@order_item_1.unit_price)
+        expect(page).to have_content(@order_item_1.subtotal)
+      end
+
+      within "#item-#{@item_2.id}" do
+        expect(page).to have_content(@item_2.name)
+        expect(page).to have_content(@item_2.description)
+        expect(page).to have_content(@item_2.image)
+        expect(page).to have_content(@order_item_2.quantity)
+        expect(page).to have_content(@order_item_2.unit_price)
+        expect(page).to have_content(@order_item_2.subtotal)
+      end
+
+      expect(page).to have_content(@order.quantity_of_items)
+      expect(page).to have_content(@order.grand_total)
+    end
   end
 end
-# I see all information about the order, including the following information:
-# - the ID of the order
-# - the date the order was made
-# - the date the order was last updated
-# - the current status of the order
-# - each item the user ordered, including name, description, thumbnail, quantity, price and subtotal
-# - the total quantity of items in the whole order
-# - the grand total of all items for that order
