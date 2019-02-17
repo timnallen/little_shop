181261ae40203ad14ccbd5169c6f092dc88402ec
Christopher Lewis <41928817+csvlewis@users.noreply.github.com>
2019-02-16 19:11:27 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get '/dashboard', to: 'merchant/users#show'
-  get '/dashboard/items', to: 'merchant/items#index'
   get '/register', to: 'users#new'
   get '/merchants', to: 'users#index'
 
