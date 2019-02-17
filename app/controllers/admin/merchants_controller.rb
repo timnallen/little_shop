7c38d79bdfdb244aea4d9bdaa5fc45ae83277f56
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-17 02:35:09 -0700

    adds redirect to user profile

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,5 +2,6 @@ class Admin::MerchantsController < Admin::BaseController
   def show
     @merchant = User.find(params[:id])
     @orders = Order.pending_orders(@merchant)
+    redirect_to admin_user_path(@merchant) if @merchant.registered?
   end
 end
