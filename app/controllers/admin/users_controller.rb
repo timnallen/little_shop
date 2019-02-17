069847cf0df2eda7129bbf74052bb9892b5deeea
plapicola <p.lapicola@gmail.com>
2019-02-16 22:40:24 -0700

    Adds redirect to admin/users#show in the event the desired user is a merchant

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < Admin::BaseController
 
   def show
     @user = User.find(params[:id])
+    redirect_to admin_merchant_path(@user) if @user.role == 'merchant'
   end
 
   def edit
