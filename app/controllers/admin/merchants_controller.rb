6f0550f6c2055cba57b2bcf8901f684fb5594a72
Chris Lewis <13chlewis@gmail.com>
2019-02-17 14:05:43 -0700

    Created test and functionality for admins downgrading merchants to registered users

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,4 +4,14 @@ class Admin::MerchantsController < Admin::BaseController
     @orders = Order.pending_orders(@merchant)
     redirect_to admin_user_path(@merchant) if @merchant.registered?
   end
+
+  def downgrade
+    @merchant = User.find(params[:id])
+    @merchant.items.each do |item|
+      item.update(disabled: 'true')
+    end
+    @merchant.update(role: 'registered')
+    flash[:primary] = 'You have downgraded a merchant'
+    redirect_to admin_user_path(@merchant)
+  end
 end
