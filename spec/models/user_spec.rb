34dc8da65f07593e969cc7657c5de088172f9993
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-18 11:03:30 -0700

    setup for slowest and fastest merchants test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -128,8 +128,71 @@ RSpec.describe User, type: :model do
   end
 
   describe 'class methods' do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @merchant_3 = create(:merchant)
+      @merchant_4 = create(:merchant)
+
+      @user_1 = create(:user, state: 'California', city: 'Los Angeles')
+      @user_2 = create(:user, state: 'Florida', city: 'Wausau')
+      @user_3 = create(:user, state: 'Wisconsin', city: 'Wausau')
+      @user_4 = create(:user, state: 'Wisconsin', city: 'Green Bay')
+
+
+      @item_1 = create(:item, user: @merchant_1, quantity: 100, price: 30)
+      @item_2 = create(:item, user: @merchant_1, quantity: 100, price: 20)
+      @item_3 = create(:item, user: @merchant_2, quantity: 100, price: 17)
+      @item_4 = create(:item, user: @merchant_3, quantity: 100, price: 5)
+      @item_5 = create(:item, user: @merchant_4, quantity: 100, price: 3)
+      @item_6 = create(:item, user: @merchant_2, quantity: 100, price: 3)
+
+      @order_1 = create(:order, user: @user_1, status: 'completed')
+
+      create(:order_item, order: @order_1, item: @item_1, unit_price: 30, quantity: 10, fulfilled: true, created_at: 60.seconds.ago, updated_at: 1.second.ago)
+      create(:order_item, order: @order_1, item: @item_2, unit_price: 20, quantity: 10, fulfilled: true, created_at: 58.seconds.ago, updated_at: 1.second.ago)
+      create(:order_item, order: @order_1, item: @item_6, unit_price: 3, quantity: 10, fulfilled: true, create_at: 31.seconds.ago, updated_at: 1.second.ago)
+      #Order 1
+      #Total: $530
+      #From Merchant 1
+      #Item 1  $30  10 = $300
+      #Item 2 $20 10 = $200
+      #Bought from Merchant 1 = $500
+      #Took him about a minute to fulfill
+
+      #From Merchant 2
+      #Item 6 $3 10 - $30
+      #Bought from Merchant 2 = $30
+      #Took him 30 seconds to fulfill
+
+      #Order 2
+      #total: $340
+      #From Merchant 2
+      #about 30 second to fulfill
+      @order_2 = create(:order, user: @user_1, status: 'pending')
+
+      create(:order_item, order: @order_2, item: @item_3, unit_price: 17, quantity: 20, fulfilled: true, created_at: 32.seconds.ago, updated_at: 1.second.ago)
+
+      #Order 3
+      #Total: $130
+      #From Merchant 3 $100
+      #Item 4 unit price: 5 quantity: 20
+      #Took them 3 seconds to fulfill
+
+      #From Merchant 4 $30
+      #Item 5 unit price 3, quantity: 10
+      #Took them 1 second to fulfill
+      #
+
+      @order_3 = create(:order, user: @user_1, status: 'completed')
+
+      create(:order_item, order: @order_3, item: @item_4, unit_price: 5, quantity: 20, fulfilled: true, created_at: 4.seconds.ago, updated_at: 1.second.ago)
+      create(:order_item, order: @order_3, item: @item_5, unit_price: 3, quantity: 10, fulfilled: true, created_at: 20.seconds.ago, updated_at: 19.seconds.ago)
+
+
+    end
     it '.all_merchants' do
-      merchants = create_list(:merchant, 3)
+      merchants = [@merchant_1, @merchant_2, @merchant_3, @merchant_4]
       merchants << build(:inactive_merchant)
       merchants.last.save
 
@@ -138,76 +201,36 @@ RSpec.describe User, type: :model do
 
     describe '.top_merchants_by_revenue' do
       it 'should return the top 3 merchants who have sold the most by price and quantity and their revenue' do
-        merchant_1 = create(:merchant)
-        merchant_2 = create(:merchant)
-        merchant_3 = create(:merchant)
-        merchant_4 = create(:merchant)
-        user_1 = create(:user)
-
-
-        item_1 = create(:item, user: merchant_1, quantity: 100, price: 30)
-        item_2 = create(:item, user: merchant_1, quantity: 100, price: 20)
-        item_3 = create(:item, user: merchant_2, quantity: 100, price: 17)
-        item_4 = create(:item, user: merchant_3, quantity: 100, price: 5)
-        item_5 = create(:item, user: merchant_4, quantity: 100, price: 3)
-        item_6 = create(:item, user: merchant_5, quantity: 100, price: 2)
-
-        order_1 = create(:order, user: user_1, status: 'completed')
-
-        create(:order_item, order: order_1, item: item_1, unit_price: 30, quantity: 10, fulfilled: true)
-        create(:order_item, order: order_1, item: item_2, unit_price: 20, quantity: 10, fulfilled: true)
-        create(:order_item, order: order_1, item: item_6, unit_price: 3, quantity: 10, fulfilled: true)
-        #Order 1
-        #Total: $530
-        #From Merchant 1
-        #Item 1  $30  10 = $300
-        #Item 2 $20 10 = $200
-        #Bought from Merchant 1 = $500
-
-        #From Merchant 2
-        #Item 6 $3 10 - $30
-        #Bought from Merchant 2 = $30
-
-        #Order 2
-        #total: $340
-        #From Merchant 2
-        order_2 = create(:order, user: user_1, status: 'pending')
-
-        create(:order_item, order: order_2, item: item_3, unit_price: 17, quantity: 20, fulfilled: true)
-
-        #Order 3
-        #Total: $130
-        #From Merchant 3 $100
-        #Item 4 unit price: 5 quantity: 20
-
-        #From Merchant 4 $30
-        #Item 5 unit price 3, quantity: 10
-        #
-        #
-
-        order_3 = create(:order, user: user_1, status: 'completed')
-
-        create(:order_item, order: order_3, item: item_4, unit_price: 5, quantity: 20, fulfilled: true)
-        create(:order_item, order: order_3, item: item_5, unit_price: 3, quantity: 10, fulfilled: true)
-
 
         expect(User.top_merchants_by_revenue).to eq([merchant_1, merchant_2, merchant_3])
         expect(User.top_merchants_by_revenue[0].revenue).to eq(500)
         expect(User.top_merchants_by_revenue[1].revenue).to eq(370)
         expect(User.top_merchants_by_revenue[2].revenue).to eq(100)
-
       end
     end
 
-    describe '.top_merchants_by_speed' do
+    describe '.fastest_merchants' do
       it 'should return the top 3 merchants who were fastest at fulfilling items in an order, and their times' do
+        #fastest merchants should be, in order: merchant_4, merchant_3, merchant_2
+        expect(User.fastest_merchants).to eq([@merchant_4, @merchant_3, @merchant_2])
+        expect(User.fastest_merchants[0].time).to eq("00:00:01")
+        expect(User.fastest_merchants[1].time).to eq("00:00:03")
+        expect(User.fastest_merchants[2].time).to eq("00:00:30")
+
       end
 
       it 'should return the worst 3 merchants who were slowest at fulfilling items in an order, and their times' do
+        #slowest merchants should be, in order: merchant_1, merchant_2, merchant_3
+        expect(User.slowest_merchants).to eq([@merchant_1, @merchant_2, @merchant_3])
+        expect(User.fastest_merchants[0].time).to eq("00:00:57")
+        expect(User.fastest_merchants[1].time).to eq("00:00:30")
+        expect(User.fastest_merchants[2].time).to eq("00:00:03")
       end
     end
     describe '.top_states' do
       it 'should return the top 3 states where any orders were shipped (by number of orders), and count of orders' do
+        
+
       end
     end
     describe '.top_cities' do
@@ -217,7 +240,7 @@ RSpec.describe User, type: :model do
 
     describe '.biggest_orders' do
       it 'should return the top 3 biggest orders by quantity of items shipped in an order, plus their quantities' do
-      end 
+      end
     end
   end
 end
