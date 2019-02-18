032cd621ab7a95e065502a216d5009fc531f4821
plapicola <p.lapicola@gmail.com>
2019-02-18 13:46:55 -0700

    Adds admin ability to manipulate orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,12 @@ class Admin::OrdersController < Admin::BaseController
 
   def show
     @order = Order.find(params[:id])
-    
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.cancel
+    flash[:danger] = "Order ##{@order.id} has been cancelled."
+    redirect_to admin_user_path(@order.user_id)
   end
 end
