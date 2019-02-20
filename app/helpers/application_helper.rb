396482787a9da8f15a869e9b21b67cec2f0f3ab2
plapicola <p.lapicola@gmail.com>
2019-02-20 15:19:23 -0700

    Updates time formatting helper to strip ending whitespace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if time.include?(":")
       time.to_s[0..7].split(":").map.with_index do |time, index|
         "#{time} #{labels[index]} "
-      end.join("")
+      end.join("").rstrip
     else
       time
     end
